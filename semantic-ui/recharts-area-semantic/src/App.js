29bc1b53584fa8339f0b86cade7909acf4d78d64
alangrn <alangrn@hotmail.com>
2021-05-09 14:13:17 +0100

    area added to sandbox

---
+++ semantic-ui/recharts-area-semantic/src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+import { Card } from "semantic-ui-react";
+import Area from "./Area";
+import ButtonComponent from "./Button";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="ui two column centered grid" style={{ margin: "20px" }}>
+        <Card fluid>
+          <Card.Content>
+            <Area />
+            <ButtonComponent />
+          </Card.Content>
+        </Card>
+      </div>
+    </div>
+  );
+}
