2a44608dc2e12adcec1e19c7eb264a853bddb9a6
alangrn <alangrn@hotmail.com>
2021-05-09 11:14:15 +0100

    table added

---
+++ semantic-ui/motor-table-semantic/src/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useButton } from '@motor-js/engine'
+import { Button } from 'semantic-ui-react'
+
+const ButtonComponent = () => {
+
+  const { clearSelections } = useButton()
+
+  return ( 
+    <Button onClick={clearSelections}>
+      Clear Selections
+    </Button>
+  )
+}
+
+export default ButtonComponent
