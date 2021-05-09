1d013cd1fe32612907c01b3f868e68a1b1108f07
alangrn <alangrn@hotmail.com>
2021-05-09 10:53:56 +0100

    Codesandboxes start

---
+++ semantic-ui/recharts-pie-semantic/src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+// import Table from "./Table";
+import Pie from "./Pie";
+import ButtonComponent from "./Button";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ButtonComponent />
+      <Pie />
+    </div>
+  );
+}
---
+++ semantic-ui/table-semantic/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Table from './Table'
+import ButtonComponent from './Button'
+
+export default function App() {
+  return (
+    <div className="App">
+      <ButtonComponent />
+      <Table />
+    </div>
+  );
+}
