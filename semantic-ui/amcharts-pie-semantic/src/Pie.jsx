4e638abac9913e876cf39283870d44585aa1f00b
alangrn <alangrn@hotmail.com>
2021-05-10 19:33:42 +0100

    Pie chart connected to motor

--- semantic-ui/amcharts-pie-semantic/src/Pie.jsx
+++ semantic-ui/amcharts-pie-semantic/src/Pie.jsx
@@ -52,56 +52,19 @@ const PieExampleCompact = () => {
     let x = am4core.create("chartdiv", am4charts.PieChart);
 
     // Add data
-    x.data = [
-      {
-        country: "Lithuania",
-        litres: 501.9,
-      },
-      {
-        country: "Czech Republic",
-        litres: 301.9,
-      },
-      {
-        country: "Ireland",
-        litres: 201.1,
-      },
-      {
-        country: "Germany",
-        litres: 165.8,
-      },
-      {
-        country: "Australia",
-        litres: 139.9,
-      },
-      {
-        country: "Austria",
-        litres: 128.3,
-      },
-      {
-        country: "UK",
-        litres: 99,
-      },
-      {
-        country: "Belgium",
-        litres: 60,
-      },
-      {
-        country: "The Netherlands",
-        litres: 50,
-      },
-    ];
+    x.data = data;
 
     // Add and configure Series
     let pieSeries = x.series.push(new am4charts.PieSeries());
-    pieSeries.dataFields.value = "litres";
-    pieSeries.dataFields.category = "country";
+    pieSeries.dataFields.value = "Revenue";
+    pieSeries.dataFields.category = "Category";
 
     chart.current = x;
 
     return () => {
       x.dispose();
     };
-  }, []);
+  }, [dataSet]);
 
   return <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>;
 };
