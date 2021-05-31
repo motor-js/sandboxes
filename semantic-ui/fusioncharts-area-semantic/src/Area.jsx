5f5fd59cb761d18967f522424dc742657efb2a32
alangrn <alangrn@hotmail.com>
2021-05-31 09:02:50 +0100

    fusioncharts area added

---
+++ semantic-ui/fusioncharts-area-semantic/src/Area.jsx
@@ -0,0 +1,90 @@
+import FusionCharts from "fusioncharts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import ReactFC from "react-fusioncharts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+import { useData } from "@motor-js/engine";
+
+const LineExampleCompact = () => {
+  const paletteColors = [
+    "#B03060",
+    "#FE9A76",
+    "#FFD700",
+    "#32CD32",
+    "#016936",
+    "#008080",
+    "#0E6EB8",
+    "#EE82EE",
+    "#B413EC",
+    "#FF1493",
+    "#A52A2A",
+    "#A0A0A0",
+    "#000000",
+  ];
+
+  const cols = [
+    {
+      qField: "[OrderDate]",
+      qLabel: "OrderDate",
+    },
+    {
+      qField: "=sum(Quantity * Price)",
+      qLabel: "Revenue",
+    },
+  ];
+
+  const { dataSet, select } = useData({
+    cols,
+  });
+
+  const { data, valueKey, nameKey } = dataSet;
+
+  const chartData =
+    data &&
+    data.map((p) => ({
+      label: p[nameKey],
+      value: p[valueKey],
+    }));
+
+  const dataSource = {
+    chart: {
+      caption: "Categories With Most Revenue [2017-18]",
+      subCaption: "Data supplied by Motor",
+      xAxisName: nameKey,
+      yAxisName: valueKey,
+      numberSuffix: "K",
+      theme: "fusion",
+      paletteColors,
+    },
+    data: chartData,
+  };
+
+  const chartConfigs = {
+    type: "area2d",
+    width: "100%",
+    height: 400,
+    dataFormat: "json",
+    dataSource,
+  };
+
+  // Event listener for dataplotclick event on chart. Update message with data plot values.
+  const dataPlotClick = (eventObj, dataObj) => {
+    if (data && data[dataObj.dataIndex])
+      select(0, [data[dataObj.dataIndex].elemNumber], false);
+  };
+
+  FusionCharts.addEventListener("dataplotClick", dataPlotClick);
+
+  return (
+    <>
+      <div className="header">
+        <h1 className="title">Fusioncharts Area Chart</h1>
+      </div>
+      <ReactFC {...chartConfigs} />
+    </>
+  );
+};
+
+export default LineExampleCompact;
