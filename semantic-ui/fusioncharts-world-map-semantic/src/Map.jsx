c11b758729508e37b9da9361f031669595bf26df
alangrn <alangrn@hotmail.com>
2021-05-31 11:12:46 +0100

    Fusioncharts world map by country added

---
+++ semantic-ui/fusioncharts-world-map-semantic/src/Map.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import FusionCharts from "fusioncharts";
+import Maps from "fusioncharts/fusioncharts.maps";
+
+//download all map from https://cdn.fusioncharts.com/downloads/addons/fusionmaps-xt-definition.zip
+import WorldWithCountries from "fusioncharts/maps/fusioncharts.worldwithcountries";
+import ReactFC from "react-fusioncharts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+import World from "fusioncharts/maps/fusioncharts.world";
+
+ReactFC.fcRoot(FusionCharts, Maps, WorldWithCountries, FusionTheme);
+
+import { useData } from "@motor-js/engine";
+
+const PieExampleCompact = () => {
+  const paletteColors = [
+    "#B03060",
+    "#FE9A76",
+    "#FFD700",
+    "#32CD32",
+    "#016936",
+    "#008080",
+    "#0E6EB8",
+    "#EE82EE",
+    "#B413EC",
+    "#FF1493",
+    "#A52A2A",
+    "#A0A0A0",
+    "#000000",
+  ];
+
+  const cols = [
+    {
+      qField: "[CountryID]",
+      qLabel: "CountryID",
+    },
+    {
+      qField: "=sum(Quantity * Price)",
+      qLabel: "Revenue",
+    },
+  ];
+
+  const { dataSet, select } = useData({
+    cols,
+  });
+
+  const { data, valueKey, nameKey } = dataSet;
+
+  const chartData =
+    data &&
+    data.map((p, i) => ({
+      id: p[nameKey],
+      value: p[valueKey],
+      color: paletteColors[i],
+    }));
+
+  const dataSource = {
+    chart: {
+      showCanvasBorder: true,
+      canvasBorderColor: "#000000",
+      canvasBorderThickness: 1,
+      showBorder: true,
+      borderColor: "#000000",
+      fillColor: "#f1f1f1",
+      caption: "Wearables Revenue by Country",
+      includevalueinlabels: "1",
+      showHoverEffect: false,
+      showEntityHoverEffect: false,
+      theme: "fusion",
+    },
+    data: chartData,
+  };
+
+  const chartConfigs = {
+    type: "worldwithcountries",
+    width: "100%",
+    height: 400,
+    dataFormat: "json",
+    dataSource,
+  };
+
+  return (
+    <>
+      <div className="header">
+        <h1 className="title">Fusioncharts World with Countries Chart</h1>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <ReactFC {...chartConfigs} />
+      </div>
+    </>
+  );
+};
+
+export default PieExampleCompact;
---
+++ semantic-ui/fusioncharts-world-map-semantic/src/config.js
@@ -0,0 +1,15 @@
+export const qlikConfig = {
+  //Enter your app config here..
+  host: "motor.eu.qlikcloud.com", // Qlik Sense Host
+  secure: true, // Whether your host is secure of not (HTTPS / HTTP)
+  port: null, // Qlik Sense site port
+  prefix: "", // Prefix
+  appId: "d60e5a8a-ea92-4f5b-913c-7be941f23eb0", // Application Id
+  webIntId: "4Tx-ydWxSQEM_q1ajlYBVzGgVUVJUo-i", // Web Integration Id, for connection to Qlik cloud
+  qcs: true, // whether you are connecting to a Qlik Cloud site or not
+  //host: "motor-demo.com", // Qlik Sense Host
+  //secure: true, // Whether your host is secure of not (HTTPS / HTTP)
+  //port: null, // Qlik Sense site port
+  //prefix: "", // Prefix
+  //appId: "Wearables2", // Application Id
+};
