dc244824f95c8f416bb9534e9e5b1030a0b2dd3d
alangrn <alangrn@hotmail.com>
2021-06-20 10:53:25 +0100

    Map repointed to demo app

--- semantic-ui/fusioncharts-world-map-semantic/src/config.js
+++ semantic-ui/fusioncharts-world-map-semantic/src/config.js
@@ -4,12 +4,7 @@ export const qlikConfig = {
   secure: true, // Whether your host is secure of not (HTTPS / HTTP)
   port: null, // Qlik Sense site port
   prefix: "", // Prefix
-  appId: "d60e5a8a-ea92-4f5b-913c-7be941f23eb0", // Application Id
+  appId: "0294cf88-eb02-484a-b315-cf06b45ac347", // Application Id
   webIntId: "4Tx-ydWxSQEM_q1ajlYBVzGgVUVJUo-i", // Web Integration Id, for connection to Qlik cloud
   qcs: true, // whether you are connecting to a Qlik Cloud site or not
-  //host: "motor-demo.com", // Qlik Sense Host
-  //secure: true, // Whether your host is secure of not (HTTPS / HTTP)
-  //port: null, // Qlik Sense site port
-  //prefix: "", // Prefix
-  //appId: "Wearables2", // Application Id
 };
