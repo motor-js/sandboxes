a1fb7194f268782021b2f03a04d19bcbe2904094
alangrn <alangrn@hotmail.com>
2021-07-15 16:49:04 +0100

    updated variables

--- semantic-ui/slider-with-statistics-semantic/src/Slider.jsx
+++ semantic-ui/slider-with-statistics-semantic/src/Slider.jsx
@@ -4,21 +4,24 @@ import { Label } from "semantic-ui-react";
 
 import { useVariable } from "@motor-js/engine";
 
+const start = [2, 12];
+const varName = "testVar4";
+
 const SliderExample = () => {
   const { value, setProperties } = useVariable({
-    qName: "newVar2",
-    qDefinition: "3,5",
+    qName: varName,
+    qDefinition: start.toString(),
   });
 
-  const [multipleValues, setMultipleValues] = useState([2, 12]);
+  const [multipleValues, setMultipleValues] = useState(start);
   const settings = {
-    start: [2, 12],
+    start,
     min: 0,
     max: 25,
     step: 1,
     onChange: (value) => {
       setMultipleValues(value);
-      setProperties({ qId: "test123", qDefinition: value.toString() });
+      setProperties({ qName: varName, qDefinition: value.toString() });
     },
   };
 
