ff42d03e4382604355263e9486d52861c98d6046
alangrn <alangrn@hotmail.com>
2021-08-07 12:52:51 +0100

    nebula sandbox added

---
+++ semantic-ui/nebula-bar-semantic/src/Nebula.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { NebulaContainer } from "@motor-js/nebula";
+
+const Nebula = () => {
+  return (
+    <NebulaContainer
+      render={{ id: "jVfmGat" }}
+      styles={{ width: "80%", height: 400, paddingTop: 50 }}
+    />
+  );
+};
+
+export default Nebula;
