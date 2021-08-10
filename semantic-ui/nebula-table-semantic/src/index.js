720861bb112877fd5f8d7ce5811044f9b5f3816d
alangrn <alangrn@hotmail.com>
2021-08-10 01:07:24 +0100

    nebula table example

--- semantic-ui/nebula-table-semantic/src/index.js
+++ semantic-ui/nebula-table-semantic/src/index.js
@@ -2,12 +2,15 @@ import ReactDOM from "react-dom";
 import { Motor } from "@motor-js/engine";
 import { qlikConfig } from "./config";
 import App from "./App";
+import { NebulaConnection } from "@motor-js/nebula";
 import "semantic-ui-css/semantic.min.css";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <Motor config={qlikConfig}>
-    <App />
+    <NebulaConnection>
+      <App />
+    </NebulaConnection>
   </Motor>,
   rootElement
 );
